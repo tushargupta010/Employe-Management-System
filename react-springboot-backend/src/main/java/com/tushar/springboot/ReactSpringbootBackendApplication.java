2b19528a1f3f1e042cc5914229778c78240c254b
tushar <tushargupta010@gmail.com>
2021-12-28 08:49:34 +0530

    react-springboot-backend

---
+++ react-springboot-backend/src/main/java/com/tushar/springboot/ReactSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tushar.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ react-springboot-backend/src/test/java/com/tushar/springboot/ReactSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
